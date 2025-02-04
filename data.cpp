190adafaea50338ef2998e8ea7154d8dfbe1b0b9
jatinparmar24 <jatinparmar2424@gmail.com>
2025-02-04 20:38:45 +0530

    merge

--- data.cpp
+++ data.cpp
@@ -2554,6 +2554,8 @@ for(int i=0;i<s;++i){
 // ans arrange arr in ascending order
 
 
+// 4 = Quick Sort
+
 #include <iostream>
 using namespace std;
 int part(int arr[] , int low , int high)
@@ -2614,6 +2616,76 @@ for(int i=0;i<s;i++)
 }
 // ans arrange arr in ascending order
 
+
+// 5 = Merge Sort
+
+
+#include <iostream>
+using namespace std;
+void combine(int arr[],int low,int mid,int high)
+{
+    int i=low;
+    int j=mid+1;
+    int k=low;
+    int bk[high+1];
+    while(i<=mid && j<=high)
+    {
+        if(arr[i]<arr[j])
+        {
+            bk[k]=arr[i];
+            i++;
+            k++;
+        }
+        else{
+            bk[k]=arr[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<=mid)
+    {
+        bk[k]=arr[i];
+        i++;
+        k++;
+    }
+    while(j<=high)
+    {
+        bk[k]=arr[j];
+        j++;
+        k++;
+    }
+    for(int i=low;i<k; i++)
+    {
+        arr[i]=bk[i];
+    }
+}
+void mergesrt(int arr[],int low,int high)
+{
+    int mid;
+    if(low<high)
+    {
+        mid=(low+high)/2;
+        mergesrt(arr,low,mid);
+        mergesrt(arr,mid+1,high);
+        combine(arr,low,mid,high);
+    }
+}
+
+
+int main() {
+  int arr[]={9,2,3,1,6,5};
+  int s = sizeof(arr)/sizeof(arr[0]);
+  for(int i=0;i<s;i++)
+  {
+      cout<<arr[i]<<"\t";
+  }
+  cout<<"\n after merge sort\n";
+  mergesrt(arr,0,s-1);
+  for(int i=0;i<s;i++)
+  {
+      cout<<arr[i]<<"\t";
+  }
+}
 
 
 
