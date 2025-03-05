15262513e8995adde2ca7c65dcffcf5aa0c51691
jatinparmar24 <jatinparmar2424@gmail.com>
2025-03-05 11:17:24 +0530

    add vector

--- data.cpp
+++ data.cpp
@@ -3156,3 +3156,67 @@ int main() {
     }
     
 }
+
+
+
+// write a programme to sum two vector = 
+
+
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main() {
+    vector<int>v1{3,1,2,5};
+    vector<int>v2{7,2,3,4};
+    for(int i=0;i<v1.size();i++){
+        cout<<v1[i]+v2[i]<<"\t";
+    }
+
+}
+// ans = 10 , 3 , 5 , 9
+
+
+// add two vector with one variable =
+
+
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main() {
+    
+    int sum=0;
+    vector<int>v1{3,1,2,5};
+    vector<int>v2{7,2,3,4};
+    for(int i=0;i<v1.size();i++){
+        sum+=v1[i]+v2[i];
+    }
+    cout<<sum;
+
+}
+
+// 27
+
+// add two vector with 2 variables= 
+
+
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main() {
+    
+    int sum1=0;
+    int sum2=0;
+    vector<int>v1{3,1,2,5};
+    vector<int>v2{7,2,3,4};
+    for(int i=0;i<v1.size();i++){
+        sum1=v1[i]+sum1;
+        sum2=v2[i]+sum2;
+    }
+    cout<<sum1+sum2;
+
+}
+
+// ans = 27 
