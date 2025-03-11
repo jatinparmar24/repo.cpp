ca7437833c38c3ca45a2a804fb5c9f59eeb48836
jatinparmar24 <jatinparmar2424@gmail.com>
2025-03-11 21:39:45 +0530

    stack pop and top

--- data.cpp
+++ data.cpp
@@ -3476,3 +3476,70 @@ int main() {
     }
 
 }
+
+
+
+
+
+
+// checking stack empty or not and counting 
+
+#include <iostream>
+using namespace std;
+class stacks
+{
+ public:int *arr;
+ int n,top;
+ stacks(int s){
+    n=s;
+    arr=new int[n];
+    top=-1;
+ }
+
+ void pushs(int a){
+    if(top==n-1){
+        cout<<"overflow\n";
+        return;
+    }
+    top++;
+    arr[top]=a;
+ }
+
+ int pops(){
+    if(top==-1){
+        cout<<"underflow\n";
+        return -1;
+    }
+    return top--;
+ }
+
+ int tops(){
+    if(top==-1){
+        cout<<"there is no element";
+        return -1;
+    }
+    return arr[top];
+ }
+
+};
+int main()
+{
+    int val;
+    cout<<"Enter Size = ";
+    cin>>val;
+    int c;
+    stacks st(val);
+    for(int i=1;i<=val;i++)
+    {
+        cin>>c;
+        st.pushs(c);
+    }
+    while(st.top!=-1)
+    {
+        cout<<st.tops()<<"\n";
+        st.pops();
+    }
+    cout<<"top value = "<<st.tops()<<"\n";
+    st.pops();
+    cout<<"top value = "<<st.tops()<<"\n";
+}
